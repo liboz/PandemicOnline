6c4c16cff77adefa9c6bbb8179c1d5e225a1576f
Libo Zeng <ringil.sword@gmail.com>
2019-08-10 00:06:23 -0400

    initial send to frontend

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
 
 const express = require('express')
 const app = express()
+const socketIO = require('socket.io')
+var cors = require('cors')
+
+
 const seedrandom = require('seedrandom');
+const cities = require('./data/cities')
+const game = require('./game')
+
+let curr_game = null
+
+//console.log(games[0])
 
 //middlewares
 app.use(express.static('public'))
 
+app.use(cors())
 
 //routes
 app.get('/', (req, res) => {
-	res.send("hello world")
+	res.send(curr_game)
 })
 
 //Listen on port 3000
-server = app.listen(3000)
+const server = app.listen(3000)
+
+const io = socketIO(server);
+io.set('transports', ['websocket']);
 
 
-const cities = require('./data/cities')
-const game = require('./game')
-
-const games = []
-let myrng = seedrandom();
-games.push(new game.Game(cities, myrng))
-//console.log(games[0])
-
-function play(game) {
-	game.initialize_board()
-	while (true) {
-
-	}
-};
+io.on('connection', function (socket) {
+	console.log('a user connected');
+	curr_game = new game.Game(cities)
+	socket.emit("new game", curr_game);
+	socket.on('disconnect', function () {
+		console.log('user disconnected');
+		curr_game = null
+	});
+});
