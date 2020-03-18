9beeffa8dbef3a2ac3d4f485d4dea2777f4ea33a
Libo Zeng <ringil.sword@gmail.com>
2020-03-17 23:13:34 -0400

    just use node directly

--- publish.bash
+++ publish.bash
@@ -1,4 +1,4 @@
 cp app.yaml dist/app.yaml
 cp package.json dist/package.json
-sed -i "/\"start\":/c\\\"start\": \"pm2 start server.js\"" dist/package.json
+sed -i "/\"start\":/c\\\"start\": \"node server.js\"" dist/package.json
 cat dist/package.json
