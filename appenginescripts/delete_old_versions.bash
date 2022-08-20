eddd4adda3f9339400e16d8b08f9975f470846e4
Libo Zeng <ringil.sword@gmail.com>
2022-08-20 16:36:08 -0400

    Delete appenginescripts directory

--- appenginescripts/delete_old_versions.bash
+++
@@ -1,9 +0,0 @@
-gcloud app versions list --sort-by "~LAST_DEPLOYED" | tail -n +5 | while read detail
-do
-  SERVING_STATUS=`echo ${detail} | awk '{print $5}'`
-  VERSION=`echo ${detail} | awk '{print $2}'`
-  if [ "${SERVING_STATUS}" = 'STOPPED' ]
-  then
-    gcloud app versions delete ${VERSION} --quiet
-  fi
-done
